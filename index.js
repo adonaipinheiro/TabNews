8972834eb9fe0a88e8e716eeb4c5154ffa0ea982
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2023-06-12 00:14:03 -0300

    chore: Prevenção contra JailBreak

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
  * @format
  */
 import 'react-native-gesture-handler';
+import JailBreak from 'jail-monkey';
 import {AppRegistry} from 'react-native';
+import ExitApp from 'react-native-exit-app';
 
 import {name as appName} from './app.json';
 import App from './src';
 
-AppRegistry.registerComponent(appName, () => App);
+if (JailBreak.isJailBroken() || JailBreak.isOnExternalStorage()) {
+  ExitApp.exitApp();
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+}
